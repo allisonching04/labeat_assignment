251af9cee57d5379b18edae764c170dbccb77ff7
allisonlcching <allisonlcccc@gmail.com>
2018-01-27 14:21:17 -0700

    template done

--- includes/nav.php
+++ includes/nav.php
@@ -1,9 +1,9 @@
 <nav>
 	<ul>
-		<li><a href="index.php">LOGO</a></li>
+		<li><a href="index.php"><span class="logo">LA.</span><span class="logo logo_pt2 red_title">BEAT</span></a></li>
 		<li <?php if($current == 'home'){echo 'class="current"';} ?> class="functin_nav line_nav"><a href="index.php">Home</a>
 		</li>
-		<li class="functin_nav line_nav two_nav"><a href="aboutus.php">ABout Us</a></li>
+		<li class="functin_nav line_nav two_nav"><a href="aboutus.php">About Us</a></li>
 		<li class="functin_nav line_nav three_nav"><a href="musicbeat.php">MusicBest</a></li>
 		<li class="functin_nav line_nav four_nav"><a href="musicguide.php">LiveBeat</a></li>
 	</ul>
